b83cf3aa4071754e81e6f62f8b1a2362b34900a4
sabariask <sabariask1307@gmail.com>
2021-04-14 20:22:52 +0530

    Debug commit

--- resume/settings.py
+++ resume/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.runserver_nostatic',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
