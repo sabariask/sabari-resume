63ecffa430a61e08c0029fcbb78181ed595ef8f9
Sabarinathan M <60172702+sabariask@users.noreply.github.com>
2021-04-16 12:51:18 +0530

    Update settings.py

--- resume/settings.py
+++ resume/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
